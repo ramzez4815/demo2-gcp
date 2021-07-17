72bece6467233f91f474e4e577fe6d21d8d248f1
ram <ubuntu@EPMXGUAW0622.localdomain>
2021-07-17 13:59:01 -0500

    Updating sql instance name

--- sql.tf
+++ sql.tf
@@ -1,5 +1,11 @@
+resource "random_string" "four_chars" {
+  length  = 4
+  upper   = false # instance names can only have lowercase letters, numbers, and hyphens
+  special = false
+}
+
 resource "google_sql_database_instance" "master" {
-    name = "demo3"
+    name = "demo-${random_string.four_chars.result}"
     database_version = "MYSQL_5_6"  #defaul 5.6
     region = var.gcp_region
     settings {
