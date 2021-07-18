f96564c1247e3130aa7e34b9c6e302c2a861509b
ram <ubuntu@EPMXGUAW0622.localdomain>
2021-07-18 18:51:53 -0500

    updating names

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs"{
-    bucket      = var.backend_bucket
-    prefix      = var.backend_bucket_prefix
+    bucket  = "tf-state-task02"
+    prefix  = "terraform/state"
   }
 }
